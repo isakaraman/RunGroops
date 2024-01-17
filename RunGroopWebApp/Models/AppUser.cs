48801b0da519410e87b95415c5051886a85aad3f
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-01-17 22:55:27 +0300

    first commit

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
